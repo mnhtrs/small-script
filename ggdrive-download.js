8abb74f3497e4338a3aaa78df030fc05b23ed940
Minh Tris <70361985+Tcoder206@users.noreply.github.com>
2024-07-07 14:03:48 +0700

    Rewrite ggdrive-download.js

--- ggdrive-download.js
+++ ggdrive-download.js
@@ -1,20 +1,19 @@
 let jspdf = document.createElement("script");
 jspdf.onload = () => {
-    let pdf = new jsPDF();
-    let images = document.getElementsByTagName("img");
-    for (let i in images) {
-        let img = images[i];
-        if (!/^blob:/.test(img.src)) continue;
-        let cvs = document.createElement('canvas');
-        let ctx = cvs.getContext('2d');
-        cvs.width = img.width*1.25;
-        cvs.height = img.height;
-        ctx.drawImage(img, 0, 0);
-        let imgData = cvs.toDataURL("image/jpeg", 1.0);
-        pdf.addImage(imgData, 'JPEG', 0, -7);
-        pdf.addPage();
-    }
-    pdf.save(document.title.split('.pdf - ')[0]+".pdf");
-};
-jspdf.src = 'https://gdrive.vip/wp-content/uploads/2020/jspdf.debug.js';
+	let pdf = new jsPDF();
+	let elements = document.getElementsByTagName("img");
+	for(let i in elements){
+		let img = elements[i];
+		if (!/^blob:/.test(img.src)) continue;
+		let cvs = document.createElement("canvas");
+		let ctx = cvs.getContext("2d");
+		cvs.width = img.width;
+		cvs.height = img.height;
+		ctx.drawImage(img, 0, 0,img.width, img.height);
+		pdf.addImage(cvs.toDataURL("image/jpeg", 1.0), 'JPEG', 0, 0);
+		pdf.addPage();
+	}
+	pdf.save( "download.pdf" );
+}
+jspdf.src = 'https://cdnjs.cloudflare.com/ajax/libs/jspdf/1.3.2/jspdf.min.js' ;
 document.body.appendChild(jspdf);
