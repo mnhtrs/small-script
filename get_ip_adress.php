2abd395b63aa49b7dc37d31fd11c712b5810fed9
Minh Tris <70361985+Tcoder206@users.noreply.github.com>
2024-08-25 08:37:07 +0700

    Create get_ip_adress.php

---
+++ get_ip_adress.php
@@ -0,0 +1,25 @@
+// Lấy IP Address của người dùng đang truy cập
+
+<?php
+ 
+function getClientIp () {
+    $ipaddress = '';
+ 
+    if (isset($_SERVER['HTTP_CLIENT_IP'])) {
+        $ipaddress = $_SERVER['HTTP_CLIENT_IP'];
+    } elseif (isset($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+        $ipaddress = $_SERVER['HTTP_X_FORWARDED_FOR'];
+    } elseif (isset($_SERVER['HTTP_X_FORWARDED'])) {
+        $ipaddress = $_SERVER['HTTP_X_FORWARDED'];
+    } elseif (isset($_SERVER['HTTP_FORWARDED_FOR'])) {
+        $ipaddress = $_SERVER['HTTP_FORWARDED_FOR'];
+    } elseif (isset($_SERVER['HTTP_FORWARDED'])) {
+        $ipaddress = $_SERVER['HTTP_FORWARDED'];
+    } elseif (isset($_SERVER['REMOTE_ADDR'])) {
+        $ipaddress = $_SERVER['REMOTE_ADDR'];
+    }
+ 
+    return $ipaddress;
+}
+ 
+$myIp = getClientIp();
